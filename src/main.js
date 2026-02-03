1fba48a32a6f809d2d0f0ccb3967c58b2bd2019f
Crestedgaming <jonsson_markus@hotmail.com>
2026-02-03 17:18:47 +0100

    update 1

--- src/main.js
+++ src/main.js
@@ -1,1 +1,23 @@
 import './styles/style.scss';
+
+const incomeForm = document.getElementById('income-form');
+
+incomeForm.addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  const amount = Number(document.getElementById("income-amount").value);
+  const description = document.getElementById("income-description").value;
+  const category = document.getElementById("income-category").value;
+  console.log(amount, description, category);
+
+});
+
+const income = {
+  amount: Number(document.getElementById("income-amount").value),
+  description: document.getElementById("income-description").value,
+  category: document.getElementById("income-category").value
+}
+
+console.log(income);
+
+
