6803b89148976c6eec4432fafca2fe51e0df424f
Crestedgaming <jonsson_markus@hotmail.com>
2026-02-04 14:45:01 +0100

    update 2

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import './styles/style.scss';
 
+let incomes = [];
+let expenses = [];
+
 const incomeForm = document.getElementById('income-form');
 
 incomeForm.addEventListener('submit', function (event) {
@@ -18,6 +21,30 @@ const income = {
   category: document.getElementById("income-category").value
 }
 
+incomes.push(income);
 console.log(income);
 
+
+
+const expenseForm = document.getElementById('expense-form');
+
+expenseForm.addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  const amount = Number(document.getElementById("expense-amount").value);
+  const description = document.getElementById("expense-description").value;
+  const category = document.getElementById("expense-category").value;
+  console.log(amount, description, category);
+
+});
+
+const expense = {
+  amount: Number(document.getElementById("expense-amount").value),
+  description: document.getElementById("expense-description").value,
+  category: document.getElementById("expense-category").value
+}
+
+
+expenses.push(expense);
+console.log(expense);
 
